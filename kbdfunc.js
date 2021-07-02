8d37d1f22c4fcb4484952dd4c2aeb77d7a1844e1
chaoticgoodangel <86856761+chaoticgoodangel@users.noreply.github.com>
2021-07-02 16:45:06 -0500

    Add files via upload

--- kbdfunc.js
+++ kbdfunc.js
@@ -1,23 +1,3 @@
-/*let A = new Audio("white_keys/A.mp3");
-let S = new Audio("white_keys/S.mp3");
-let D = new Audio("white_keys/D.mp3");
-let F = new Audio("white_keys/F.mp3");
-let G = new Audio("white_keys/G.mp3");
-let H = new Audio("white_keys/H.mp3");
-let J = new Audio("white_keys/J.mp3");*/
-let A = [];
-let S = [];
-let D = [];
-let F = [];
-let G = [];
-let H = [];
-let J = [];
-let W = [];
-let E = [];
-let T = [];
-let Y = [];
-let U = [];
-
 document.addEventListener("keydown", function(e) {
     if(e.code == "KeyA") {
         let A = new Audio("white_keys/A.mp3");
@@ -59,31 +39,3 @@ document.addEventListener("keydown", function(e) {
         console.log("Warning: Key not recognized.");
     }
 });
-
-/*
-document.addEventListener("keyup", function(e) {
-    if(e.code == "KeyA") {
-        console.log("The 'A' key is released.");
-        A.pause();
-    } else if (e.code == "KeyS") {
-        console.log("The 'S' key is released.");
-        S.pause();
-    } else if (e.code == "KeyD") {
-        console.log("The 'D' key is released.");
-        D.pause();
-    } else if (e.code == "KeyF") {
-        console.log("The 'F' key is released.");
-        F.pause();
-    } else if (e.code == "KeyG") {
-        console.log("The 'G' key is released.");
-        G.pause();
-    } else if (e.code == "KeyH") {
-        console.log("The 'H' key is released.");
-        H.pause();
-    } else if (e.code == "KeyJ") {
-        console.log("The 'J' key is released.");
-        J.pause();
-    } else {
-        console.log("Unrecognized key released.");
-    }
-});*/
